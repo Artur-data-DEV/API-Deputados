cb1b285319cc76a313dea215757873b082a4f8d3
Artur <arturcamposba@gmail.com>
2021-11-11 11:32:57 -0300

    WIP

--- src/pages/DeputadosDetalhes.jsx
+++ src/pages/DeputadosDetalhes.jsx
@@ -3,6 +3,7 @@ import { Card, CardImg, Col, Row } from 'react-bootstrap'
 import Cartao from '../components/Cartao.jsx'
 import apiCamara from '../services/apiCamara.js'
 import Money from "../assets/icons/money.png"
+import "../Style/Style-DeputadosDetalhes.css"
 
 const DeputadosDetalhes = (props) => {
 
@@ -10,24 +11,32 @@ const DeputadosDetalhes = (props) => {
 
     const [deputados, setDeputados] = useState({})
     const [despesas, setDespesas] = useState([])
+    const [partido, setPartido] = useState({})
+    const [idPartido, setIdPartido] = useState(0)
+    
 
     useEffect(() => {
         const id = props.match.params.id
 
         apiCamara.get(`/deputados/${id}`).then(resultado => {
             setDeputados(resultado.data.dados);
-        })
 
+            const urlSplitted = new String(resultado.data.dados.ultimoStatus.uriPartido).split('/')
+            const lastIndex = urlSplitted.length - 1
+
+            const idPartido = urlSplitted[lastIndex]
+            setIdPartido(idPartido)
+            
+            apiCamara.get(`/partidos/${idPartido}`).then(partido => {
+                setPartido(partido.data.dados)
+
+            })
+        })
         apiCamara.get(`/deputados/${id}/despesas`).then(resultado => {
             setDespesas(resultado.data.dados);
+
         })
-
     }, [props])
-
-
-
-
-    console.log(despesas);
 
     return (
 
@@ -35,31 +44,33 @@ const DeputadosDetalhes = (props) => {
 
             {deputados.id &&
                 <>
-                    <Row>
-                        <Col md={3}  style={{ backgroundColor: "black", paddingTop: "10px" }}>
+                    <Row className="Row1">
+                        <Col md={3} className="Col1">
                             <Card>
                                 <CardImg variant="top" src={deputados.ultimoStatus.urlFoto} />
                             </Card>
                         </Col>
-                        <Col md={9} style={{ backgroundColor: "silver", paddingTop: "10px" }}>
+                        <Col md={8} className="Col2">
                             <Cartao >
                                 <h1>{deputados.nomeCivil}</h1>
                                 <p>UF: {deputados.ufNascimento}</p>
                                 <p>Data de nascimento: {deputados.dataNascimento}</p>
                                 <p>Sexo: {deputados.sexo}</p>
                                 <p>Partido: {deputados.ultimoStatus.siglaPartido}</p>
+                               <p>Logo: <a href={`/partidos/${idPartido}`}><img src={partido.urlLogo} alt="Logo partido" href /></a></p>
                             </Cartao>
                         </Col>
-                        <div style={{ backgroundColor: "black" }}>
+                        <div style={{ backgroundColor: "grey" }}>
                             <Row className="mt-4">
-                                <h2 style={{color: "green", textAlign: "center"}}>GASTOS </h2> {despesas.map(despesa => (
+                                <h2 style={{ color: "black", textAlign: "center" }}>GASTOS </h2> {despesas.map(despesa => (
 
                                     <Col key={despesa.mes} md={3} className="mb-3">
                                         <Cartao  >
 
-                                            <p style={{ backgroundColor: "green", borderRadius: "80px 30px", padding:"4%"}}><img src={Money} width='20px' /> {despesa.tipoDespesa} <br /> </p>
-                                            <p style={{textAlign: "center", color: "red"}}> Custo: R${despesa.valorDocumento} </p>
-                                            <p style={{textAlign: "center"}}>{despesa.mes}/{despesa.ano}</p>
+                                            <p style={{ backgroundColor: "green", borderRadius: "80px 30px", padding: "4%" }}><img src={Money} width='20px' /> {despesa.tipoDespesa} <br /> </p>
+                                            <p style={{ textAlign: "center", color: "red" }}> Custo: R${despesa.valorDocumento} </p>
+                                            <p style={{ textAlign: "center" }}>Mês/Ano: {despesa.mes}-{despesa.ano}</p>
+
                                         </Cartao>
 
                                     </Col>
@@ -70,22 +81,6 @@ const DeputadosDetalhes = (props) => {
                             </Row>
                         </div>
                     </Row>
-
-
-
-                    {/* {despesas.id &&
-                <Row>
-                    {creditos.cast.map((ator,) => (
-                        <Col key={ator.id} md={2}>
-                            <p>{ator.name}</p>
-                            <Card>
-                                <CardImg variant="top" src={`https://image.tmdb.org/t/p/w500/${ator.profile_path}`} />
-                            </Card>
-                        </Col>
-                    ))
-                    }
-                </Row>
-            } */}
 
                 </>
             }
